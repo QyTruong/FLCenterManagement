620b25d02e058345479366191a7756263f6e97e0
le-thanh-truong <lettruong123@gmail.com>
2025-12-07 22:39:42 +0700

    admin site

--- courseapp/__init__.py
+++ courseapp/__init__.py
@@ -6,7 +6,7 @@ import cloudinary.uploader
 app = Flask(__name__)
 
 app.secret_key = "$&vd@($*vsdvs@B$vs$*@$&(*$&@("
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/flcenterdb?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@localhost/flcenterdb?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app=app)
